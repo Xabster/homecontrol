e14bdc5c2f1f6f8f925a26e894134566b924a213
LarsLennon <L@rsLennon.dk>
2013-09-22 19:03:01 +0200

    Atomic Commit, changed to dynamic listing

---
+++ HelloWorld/src/com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        //int
+
+        System.out.println("Hello World!" + args[0]); // Display the string.
+
+    }
+}
